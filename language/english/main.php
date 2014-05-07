90bc749e162881ec43512e23928a858d48c71e2f
Tad <tad0616@gmail.com>
2014-05-07 15:09:31 +0800

    加入左右區域分隔線的額外設定

--- language/english/main.php
+++ language/english/main.php
@@ -31,6 +31,38 @@ define("TF_MODULE_CONFIG",	"Administration");
 define("TF_DL_TAD_THEMES",	"Please install \"Tad Themes\" module.");
 
 /**自訂設定之語系**/
+define("TF_FOOTER_IMG", "Footer backgorund image");
+define("TF_FOOTER_IMG_DESC", "Please upload a background image.");
+define("TF_FOOTER_COLOR", "Footer text color");
+define("TF_FOOTER_COLOR_DESC", "Set the text color of footer content.");
+define("TF_FOOTER_HEIGHT", "Footer height");
+define("TF_FOOTER_HEIGHT_DESC", "Set footer height.");
+define("TF_FOOTER_BGCOLOR", "Footer background color");
+define("TF_FOOTER_BGCOLOR_DESC", "Set footer background color.");
+define("TF_FOOTER_PADDING", "Footer padding");
+define("TF_FOOTER_PADDING_DESC", "top right bottom left");
 define("TF_SHOW_VAR", "Display themes vars.");
 define("TF_SHOW_VAR_DESC", "Display all variable of tad_themes module that you could use it to design theme.");
+define("TF_USE_SHADOW", "Display shadow");
+define("TF_USE_SHADOW_DESC", "");
+define("TF_FOOTER_STYLE", "Costom styles");
+define("TF_FOOTER_STYLE_DESC", "You can design footer style by CSS.");
+define("TF_LOGO_BGCOLOR", "Logo background color");
+define("TF_LOGO_BGCOLOR_DESC", "Logo background color");
+define("TF_FONT_FAMILY", "Font family");
+define("TF_FONT_FAMILY_DEFAULT","\"Helvetica Neue\",Helvetica,Arial,sans-serif");
+define("TF_FONT_FAMILY_DESC", "CSS font-family setup.ex:".TF_FONT_FAMILY_DEFAULT);
+define("TF_USE_SHADOW_LOGO", " Display logo shadow?");
+define("TF_USE_SHADOW_LOGO_DESC", "");
+define("TF_USE_CONTAINER", "user class=\"container\"?");
+define("TF_USE_CONTAINER_DESC", "Only for BootStrap mode. if \"No\", use class=\"container-fluid\".");
+define("TF_FOOTER_BG_CSS", "Footer background CSS setup.");
+define("TF_FOOTER_BG_CSS_DESC", "Footer background CSS setup");
+
+define('TF_LEFT_SPARATE','Left separate line');
+define('TF_LEFT_SPARATE_DESC','Display separate line between left block zone and main content?');
+define('TF_RIGHT_SPARATE','Right separate line');
+define('TF_RIGHT_SPARATE_DESC','Display separate line between main content and right block zone?');
+define('TF_SPARATE_STYLE','Separate line style');
+define('TF_SPARATE_STYLE_DESC','Setup separate line style.');
 ?>
