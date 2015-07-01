62a5718f03683c65bd70b37e135237bc61a185a6
Tad <tad0616@gmail.com>
2015-07-01 16:57:09 +0800

    精簡不必要檔案

--- language/english/main.php
+++ language/english/main.php
@@ -1,34 +1,34 @@
 <?php
-define("THEME_MODULE0",	"Main menu");
-define("THEME_MODULE1",	"Home");
+define("THEME_MODULE0", "Main menu");
+define("THEME_MODULE1", "Home");
 
-define("TF_USER_ID",	"Username");
-define("TF_USER_PASS",	"Password");
-define("TF_USER_S_ID",	"Username:");
-define("TF_USER_S_PASS",	"Password:");
-define("TF_USER_ENTER","Login");
-define("TF_USER_EXIT","Logout");
-define("TF_USER_ADMIN","Administration");
-define("TF_USER_BLOCK","Blocks setup");
-define("TF_THEME_ADMIN","Theme setup");
-define("TF_THEME_DEBUG",	"Enable debug");
-define("TF_THEME_UNDEBUG",	"Unable debug");
-define("TF_USER_PROFILE","Profil");
-define("TF_USER_NOTICE","Notifications");
-define("TF_USER_NEWMSG","New message: ");
-define("TF_USER_MSG","Messages");
-define("TF_USER_WELCOME",	"Hello ");
+define("TF_USER_ID", "Username");
+define("TF_USER_PASS", "Password");
+define("TF_USER_S_ID", "Username:");
+define("TF_USER_S_PASS", "Password:");
+define("TF_USER_ENTER", "Login");
+define("TF_USER_EXIT", "Logout");
+define("TF_USER_ADMIN", "Administration");
+define("TF_USER_BLOCK", "Blocks setup");
+define("TF_THEME_ADMIN", "Theme setup");
+define("TF_THEME_DEBUG", "Enable debug");
+define("TF_THEME_UNDEBUG", "Unable debug");
+define("TF_USER_PROFILE", "Profil");
+define("TF_USER_NOTICE", "Notifications");
+define("TF_USER_NEWMSG", "New message: ");
+define("TF_USER_MSG", "Messages");
+define("TF_USER_WELCOME", "Hello ");
 
-define("TF_SEARCH",	"Search");
-define("TF_OK",	"Submit");
-define("TF_USER_REGIST",	"Create account ?");
-define("TF_USER_FORGET_PASS",	"Password lost ?");
+define("TF_SEARCH", "Search");
+define("TF_OK", "Submit");
+define("TF_USER_REGIST", "Create account ?");
+define("TF_USER_FORGET_PASS", "Password lost ?");
 
-define("TF_BLOCK_CONFIG",	"Setup this block");
-define("TF_SYSTEM_CONFIG",	"Preferences");
-define("TF_SYSTEM_MODADM",	"Online install");
-define("TF_MODULE_CONFIG",	"Administration");
-define("TF_DL_TAD_THEMES",	"Please install \"Tad Themes\" module.");
+define("TF_BLOCK_CONFIG", "Setup this block");
+define("TF_SYSTEM_CONFIG", "Preferences");
+define("TF_SYSTEM_MODADM", "Online install");
+define("TF_MODULE_CONFIG", "Administration");
+define("TF_DL_TAD_THEMES", "Please install \"Tad Themes\" module.");
 
 /**自訂設定之語系**/
 define("TF_FOOTER_IMG", "Footer backgorund image");
@@ -50,8 +50,8 @@ define("TF_FOOTER_STYLE_DESC", "You can design footer style by CSS.");
 define("TF_LOGO_BGCOLOR", "Logo background color");
 define("TF_LOGO_BGCOLOR_DESC", "Logo background color");
 define("TF_FONT_FAMILY", "Font family");
-define("TF_FONT_FAMILY_DEFAULT","\"Helvetica Neue\",Helvetica,Arial,sans-serif");
-define("TF_FONT_FAMILY_DESC", "CSS font-family setup.ex:".TF_FONT_FAMILY_DEFAULT);
+define("TF_FONT_FAMILY_DEFAULT", "\"Helvetica Neue\",Helvetica,Arial,sans-serif");
+define("TF_FONT_FAMILY_DESC", "CSS font-family setup.ex:" . TF_FONT_FAMILY_DEFAULT);
 define("TF_USE_SHADOW_LOGO", " Display logo shadow?");
 define("TF_USE_SHADOW_LOGO_DESC", "");
 define("TF_USE_CONTAINER", "user class=\"container\"?");
@@ -59,10 +59,9 @@ define("TF_USE_CONTAINER_DESC", "Only for BootStrap mode. if \"No\", use class=\
 define("TF_FOOTER_BG_CSS", "Footer background CSS setup.");
 define("TF_FOOTER_BG_CSS_DESC", "Footer background CSS setup");
 
-define('TF_LEFT_SPARATE','Left separate line');
-define('TF_LEFT_SPARATE_DESC','Display separate line between left block zone and main content?');
-define('TF_RIGHT_SPARATE','Right separate line');
-define('TF_RIGHT_SPARATE_DESC','Display separate line between main content and right block zone?');
-define('TF_SPARATE_STYLE','Separate line style');
-define('TF_SPARATE_STYLE_DESC','Setup separate line style.');
-?>
+define('TF_LEFT_SPARATE', 'Left separate line');
+define('TF_LEFT_SPARATE_DESC', 'Display separate line between left block zone and main content?');
+define('TF_RIGHT_SPARATE', 'Right separate line');
+define('TF_RIGHT_SPARATE_DESC', 'Display separate line between main content and right block zone?');
+define('TF_SPARATE_STYLE', 'Separate line style');
+define('TF_SPARATE_STYLE_DESC', 'Setup separate line style.');
--- language/tchinese/main.php
+++ language/tchinese/main.php
@@ -2,33 +2,33 @@
 define("THEME_MODULE0", "主選單");
 define("THEME_MODULE1", "首頁");
 
-define("TF_USER_ID",  "請輸入帳號");
-define("TF_USER_PASS",  "請輸入密碼");
-define("TF_USER_S_ID",  "帳號：");
-define("TF_USER_S_PASS",  "密碼：");
-define("TF_USER_ENTER","登入");
-define("TF_USER_EXIT","登出");
-define("TF_USER_ADMIN","管理");
-define("TF_USER_BLOCK","區塊");
-define("TF_THEME_ADMIN","佈景");
-define("TF_THEME_DEBUG",  "開除錯");
-define("TF_THEME_UNDEBUG",  "關除錯");
-define("TF_USER_PROFILE","帳號");
-define("TF_USER_NOTICE","通知");
-define("TF_USER_NEWMSG","有新信");
-define("TF_USER_MSG","信件");
+define("TF_USER_ID", "請輸入帳號");
+define("TF_USER_PASS", "請輸入密碼");
+define("TF_USER_S_ID", "帳號：");
+define("TF_USER_S_PASS", "密碼：");
+define("TF_USER_ENTER", "登入");
+define("TF_USER_EXIT", "登出");
+define("TF_USER_ADMIN", "管理");
+define("TF_USER_BLOCK", "區塊");
+define("TF_THEME_ADMIN", "佈景");
+define("TF_THEME_DEBUG", "開除錯");
+define("TF_THEME_UNDEBUG", "關除錯");
+define("TF_USER_PROFILE", "帳號");
+define("TF_USER_NOTICE", "通知");
+define("TF_USER_NEWMSG", "有新信");
+define("TF_USER_MSG", "信件");
 define("TF_USER_WELCOME", "歡迎：");
 
 define("TF_SEARCH", "搜尋");
 define("TF_OK", "送出");
-define("TF_USER_REGIST",  "註冊");
+define("TF_USER_REGIST", "註冊");
 define("TF_USER_FORGET_PASS", "忘記密碼");
 
 define("TF_BLOCK_CONFIG", "設定本區塊");
-define("TF_SYSTEM_CONFIG",  "系統偏好設定");
-define("TF_SYSTEM_MODADM",  "線上安裝更新");
-define("TF_MODULE_CONFIG",  "模組管理區");
-define("TF_DL_TAD_THEMES",  "請安裝 Tad Themes 模組，可以讓您更進一步的調整佈景！");
+define("TF_SYSTEM_CONFIG", "系統偏好設定");
+define("TF_SYSTEM_MODADM", "線上安裝更新");
+define("TF_MODULE_CONFIG", "模組管理區");
+define("TF_DL_TAD_THEMES", "請安裝 Tad Themes 模組，可以讓您更進一步的調整佈景！");
 
 /**自訂設定之語系**/
 define("TF_FOOTER_IMG", "頁尾底圖");
@@ -50,8 +50,8 @@ define("TF_FOOTER_STYLE_DESC", "可使用CSS語法控制頁尾內容的外觀");
 define("TF_LOGO_BGCOLOR", "上方LOGO區的底色");
 define("TF_LOGO_BGCOLOR_DESC", "上方LOGO區的底色");
 define("TF_FONT_FAMILY", "主要字型設定");
-define("TF_FONT_FAMILY_DEFAULT","\"Helvetica Neue\",Helvetica,Arial,\"微軟正黑體\",sans-serif");
-define("TF_FONT_FAMILY_DESC", "輸入字型名稱，有空白的部份需要用\"\"包起來。如：".TF_FONT_FAMILY_DEFAULT);
+define("TF_FONT_FAMILY_DEFAULT", "\"Helvetica Neue\",Helvetica,Arial,\"微軟正黑體\",sans-serif");
+define("TF_FONT_FAMILY_DESC", "輸入字型名稱，有空白的部份需要用\"\"包起來。如：" . TF_FONT_FAMILY_DEFAULT);
 define("TF_USE_SHADOW_LOGO", "陰影是否包含logo");
 define("TF_USE_SHADOW_LOGO_DESC", "當logo在滑動圖文之上時，是否仍要加上陰影？");
 define("TF_USE_CONTAINER", "外框是否套用 container");
@@ -59,10 +59,9 @@ define("TF_USE_CONTAINER_DESC", "僅BootStrap模式有效。若否則採用預設的 container
 define("TF_FOOTER_BG_CSS", "頁尾底圖CSS設定");
 define("TF_FOOTER_BG_CSS_DESC", "可利用此處設定各種背景CSS設定，例如重複方式、位置...等");
 
-define('TF_LEFT_SPARATE','左區域分隔線');
-define('TF_LEFT_SPARATE_DESC','左區域和主內容間是否加上分隔線');
-define('TF_RIGHT_SPARATE','右區域分隔線');
-define('TF_RIGHT_SPARATE_DESC','右區域和主內容間是否加上分隔線');
-define('TF_SPARATE_STYLE','左右區域分隔線的樣式');
-define('TF_SPARATE_STYLE_DESC','左右區域分隔線的樣式');
-?>
+define('TF_LEFT_SPARATE', '左區域分隔線');
+define('TF_LEFT_SPARATE_DESC', '左區域和主內容間是否加上分隔線');
+define('TF_RIGHT_SPARATE', '右區域分隔線');
+define('TF_RIGHT_SPARATE_DESC', '右區域和主內容間是否加上分隔線');
+define('TF_SPARATE_STYLE', '左右區域分隔線的樣式');
+define('TF_SPARATE_STYLE_DESC', '左右區域分隔線的樣式');
